8e75b7c58fa306d2fd702ece9297dcc9e939b118
Reyna Peralta <ReynaP_03@hotmail.com>
2024-10-10 09:40:02 -0400

    Update PowerUpManager.cs

--- PowerUpManager.cs
+++ PowerUpManager.cs
@@ -2,17 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PowerUp : MonoBehaviour
+public class Paddle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    //this entire class is tutorial part except for horizontal movement
+    public bool isPlayer1;
+    public float speed;
+    public Rigidbody2D rb;
+    public Vector3 startPosition;
+
+    private float movementY;
+    private float movementX;
+
+    void Start(){
+        startPosition = transform.position;
+    }
+    // Update is called once per frame
+    void Update(){
+        if (isPlayer1){
+            movementY = Input.GetAxisRaw("Vertical");
+            movementX = Input.GetAxisRaw("Horizontal");
+        }
+        else{
+            movementY = Input.GetAxisRaw("Vertical2");
+            movementX = Input.GetAxisRaw("Horizontal2");
+        }
+
+        rb.velocity = new Vector2(movementX * speed, movementY * speed);
+    }
+    public void Reset(){
+        rb.velocity = Vector2.zero;
+        transform.position = startPosition;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnTriggerEnter2D(Collider2D collision){
+        if (collision.tag == "Ball"){
+            if (isPlayer1){
+                GameObject.Find("GameManager").GetComponent<GameManager>().Player1Hit();
+            }
+            else{
+                GameObject.Find("GameManager").GetComponent<GameManager>().Player2Hit();
+            }
+        }
     }
 }
